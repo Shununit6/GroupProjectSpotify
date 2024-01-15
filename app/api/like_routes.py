c25f24cb3492bf4524bc2ddf3d5ccaef9b1a4270
Shununit6 <unit6xuli@gmail.com>
2024-01-14 18:28:43 -0800

    like a song with current user/view all likes

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,58 +1,47 @@
 from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
-from app.models import Like
+from app.models import db, Like
 
 like_routes = Blueprint('likes', __name__)
 
 # Get all Likes
 # Require Authentication: false
 # GET /api/songs/:id/likes
-@like_routes.route('<int:id>/likes')
+@like_routes.route('/<int:id>/likes')
 def get_likes(id):
     """
     Query for all the likes of a song by song_id.
     """
-    likes = Like.query.filter_by(song_id=id)
-    # numoflikes = likes.count()
+    likes = Like.query.filter(Like.song_id==id)
 
-    return jsonify({'likes': [likes.to_dict() for like in likes]})
+    #return number of likes of a sond by id
+    # numoflikes = likes.count()
+    # return jsonify(numoflikes)
+    return jsonify({'likes': [like.to_dict() for like in likes]})
 
 # Like or unlike a song
 # A logged in user can like or unlike a song with visible confirmation without causing a refresh/redirect.
 # POST /api/songs/:id/likes
 # DELETE /api/songs/:id/likes
-@like_routes.route('<int:id>/likes', methods=['POST', 'DELETE'])
+@like_routes.route('/<int:id>/likes', methods=['POST', 'DELETE'])
 @login_required
 def post_like(id):
     """
     Creates and returns a new like.
     """
-    likebysongid = Like.query.filter_by(song_id=id)
-    likesongcurrentuser = likebysongid.query.filter_by(user_id=current_user.id)
-    if likesongcurrentuser and request.method == 'POST':
-        return "POST"
-    elif not likesongcurrentuser and request.method == 'DELETE':
-        return "DELETE"
-    # payload = request.get_json()
-    # new_album = Album(user_id=current_user.id,title = payload['title'], release_date = payload['release_date'], url = payload['url'], copyright = payload['copyright'])
-    # db.session.add(new_album)
-    # db.session.commit()
-    # return jsonify(new_album.to_dict())
-
-# # Delete an album
-# # Deletes an existing album: A logged in user may delete one of their own Albums, removing it from the list of visible Albums without causing a refresh/redirect.
-# # Require Authentication: true
-# # Require proper authorization: Album must be created by the current user
-# # DELETE /api/albums/:id
-# @album_routes.route('/<int:albumId>', methods=['DELETE'])
-# @login_required
-# def delete_album(albumId):
-#     albumbyid = Album.query.get(albumId)
-#     if not albumbyid:
-#         return {'errors': f"Album {id} does not exist."}, 400
-#     # checks if album is created by the current user
-#     if albumbyid.user_id != current_user.id:
-#         return {'errors': f"Album{id} must be created by the current user."}, 401
-#     db.session.delete(albumbyid)
-#     db.session.commit()
-#     return {'message': 'Delete successful.'}
+    likebycurrent = Like.query.filter(Like.song_id==id).filter(Like.user_id==current_user.get_id())
+    print(likebycurrent)
+    if likebycurrent.count() == 0 and (request.method == 'POST' or request.method == 'DELETE'):
+        # return "POST"
+        new_like = Like(user_id=current_user.id,song_id=id)
+        db.session.add(new_like)
+        db.session.commit()
+        return jsonify(new_like.to_dict())
+    elif likebycurrent.count() == 1 and (request.method == 'DELETE' or request.method == 'POST'):
+        # print([like.to_dict() for like in likebycurrent])
+        # return "DELETE"
+        db.session.delete(likebycurrent)
+        db.session.commit()
+        return {'message': 'Delete successful.'}
+    else:
+        return "no action"
