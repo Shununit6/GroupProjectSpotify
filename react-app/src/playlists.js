93d41d23ee1a43fa3ae50f0e419868417bbbcba3
GaimerGai <devinmrichards95@gmail.com>
2024-01-16 21:39:11 -0700

    Working on Redux store

---
+++ react-app/src/playlists.js
@@ -0,0 +1,12 @@
+const GET_ALL_PLAYLISTS = 'playlists/GET_ALL_PLAYLISTS'
+const GET_PLAYLIST = 'playlists/GET_PLAYLIST'
+const ADD_SONG_TO_PLAYLIST = 'playlists/ADD_SONG_TO_PLAYLIST'
+const REMOVE_SONG_FROM_PLAYLIST = 'playlists/REMOVE_SONG_FROM_PLAYLIST'
+const DELETE_PLAYLIST = 'playlists/DELETE_PLAYLIST'
+
+
+/**Action Creators: */
+
+const getAllPlaylists = (playlists) => {
+  
+}
