a70681e608bcfc9ce121c0b889444d4898a97230
Shununit6 <unit6xuli@gmail.com>
2024-01-18 07:15:07 -0800

    add albumsReducer likesReducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import albumsReducer from './albums';
+import likesReducer from './likes';
 
 const rootReducer = combineReducers({
   session,
+  albumsReducer,
+  likesReducer
 });
 
 
