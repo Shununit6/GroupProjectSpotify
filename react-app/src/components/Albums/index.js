f0ab66d9b6a778297bfa3f4db295598fa5c49d99
Shununit6 <unit6xuli@gmail.com>
2024-01-18 22:07:45 -0800

    Albums set up

---
+++ react-app/src/components/Albums/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+// import { Link } from 'react-router-dom';
+import { getAllAlbums } from '../../store/albums';
+// import AlbumIndexItem from '../AlbumIndexItem';
+import "./albums.css";
+
+function Albums() {
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+    const albums = useSelector((state) => state.albums);
+    useEffect(()=>{
+      dispatch(getAllAlbums()).then(()=>setIsLoaded(true))
+    }, [dispatch]);
+
+  if (!isLoaded) {
+    return (<div>Loading...</div>);
+  }
+  console.log(albums);
+  if(isLoaded){
+  return (
+    <div id="groupslistgrid">
+        {/* <h2><Link id="eventsIsNotActive" to="/events" > Events </Link>
+        <Link id="groupsIsActive" to="/groups" > Groups </Link>
+         </h2>
+         <p>Groups in San Francisco Events</p>
+         <div id="viewallgroups">
+            {Object.values(groups).map((group, index) => (
+                  <GroupIndexItem group={group} key={index}/>
+            ))}
+         </div> */}
+    </div>
+  );}
+}
+
+export default Albums;
