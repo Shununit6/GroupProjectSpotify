d16687ddf6079183dcdcb9494f5818bf2afd7288
Shununit6 <unit6xuli@gmail.com>
2024-01-21 10:45:10 -0800

    css

--- react-app/src/components/MenuLibrary/index.js
+++ react-app/src/components/MenuLibrary/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './MenuLibrary.css';
 
@@ -19,37 +19,44 @@ function MenuLibrary({ isLoaded }){
 		// </ul>
         <nav class="leftmenu">
         <div class="leftmenufirst">
-			<i class="fa-brands fa-spotify fa-2x"></i>
             <div>
-                <p>Spotify</p>
+				<i class="fa-brands fa-spotify"></i>
+				{" "} Spotify
             </div>
-			<i class="fa-solid fa-house"></i>
             <div>
-                <NavLink exact to="/">Home</NavLink>
+                <Link exact to="/">
+					<i id="fa-house" class="fa-solid fa-house"></i>
+					{" "} Home
+				</Link>
             </div>
-			<i class="fa-solid fa-magnifying-glass"></i>
             <div>
-                <p>Search</p>
+				<i class="fa-solid fa-magnifying-glass"></i>
+				{" "} Search
             </div>
 		</div>
 		<div class="leftmenulibrary">
 			<div>
-			    <p>Your Library</p>
+				<i class="fa-solid fa-headphones-simple"></i>
+				{" "}Your Library
 			</div>
-            <div>
-				<h1>Brower playlist</h1>
-				<NavLink exact to="/">View all the playlist</NavLink>
+			<div class="leftmenulibrarycreate">
+				<div class="leftmenulibrarycreate-1">
+					<h2>Brower playlist</h2>
+					<NavLink exact to="/"><button>View all the playlist</button></NavLink>
+				</div>
+				{/* {sessionUser && */}
+				<div class="leftmenulibrarycreate-1">
+					<h2>Create your album</h2>
+					<NavLink exact to="/"><button>Create Album</button></NavLink>
+				</div>
+				{/* }
+				{sessionUser && */}
+				<div class="leftmenulibrarycreate-1">
+					<h2>Create your song</h2>
+					<NavLink exact to="/"><button>Create Song</button></NavLink>
+				</div>
+				{/* } */}
 			</div>
-            {sessionUser &&
-			<div>
-				<h1>Create your album</h1>
-				<NavLink exact to="/">Create Album</NavLink>
-			</div>}
-            {sessionUser &&
-			<div>
-				<h1>Create your song</h1>
-				<NavLink exact to="/">Create Song</NavLink>
-			</div>}
 		</div>
         </nav>
 	);
