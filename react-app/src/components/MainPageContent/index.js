4d65d2134872a5d9ff6d957ae0fc47be6f154015
Shununit6 <unit6xuli@gmail.com>
2024-01-21 22:16:38 -0800

    mainpage album added

--- react-app/src/components/MainPageContent/index.js
+++ react-app/src/components/MainPageContent/index.js
@@ -1,6 +1,7 @@
 import { React } from 'react';
 import './MainPageContent.css';
 import SongIndex from '../../components/SongIndex';
+import Albums from '../Albums';
 
 function MainPageContent(){
     const num = 5;
@@ -8,8 +9,12 @@ function MainPageContent(){
         <nav>
             <div id="MainPageContentCss">
                 <p>Spotify songs</p>
-                <ul className='landingSongIndex'>
+                <ul className='MainPageFiveSong'>
                         <SongIndex num={num}/>
+                </ul>
+                <p>Spotify albums</p>
+                <ul className='MainPageFiveAlbum'>
+                        <Albums />
                 </ul>
             </div>
         </nav>
