728db0e82b5540189eeb91229f4035125368b0f6
Shununit6 <unit6xuli@gmail.com>
2024-01-25 22:07:17 -0800

    menu library

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
+// import SignupFormPage from "./components/SignupFormPage";
+// import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import CreateSong from './components/CreateSong';
@@ -56,8 +56,8 @@ function App() {
           <Route exact path="/playlists/current" component={ManagePlaylists}/>
           <Route exact path="/playlists/:playlistId(\d+)" component={PlaylistDetails}/>
           <Route exact path="/playlists" component={PlaylistIndex}/>
-          <Route path="/login" ><LoginFormPage /></Route>
-          <Route path="/signup"><SignupFormPage /></Route>
+          {/* <Route path="/login" ><LoginFormPage /></Route>
+          <Route path="/signup"><SignupFormPage /></Route> */}
         </Switch>
       )}
     </>
