d3fae0b62f7545b601a6c9de841b8fb22081df98
GaimerGai <devinmrichards95@gmail.com>
2024-01-27 12:44:20 -0700

    add information to albums and playlists and remove delete button if user is not owner

--- react-app/src/components/AlbumDetails/index.js
+++ react-app/src/components/AlbumDetails/index.js
@@ -2,7 +2,7 @@ import './AlbumDetails.css';
 import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAlbumDetails} from '../../store/albums';
+import { getAlbumDetails } from '../../store/albums';
 import DeleteAlbumModal from '../DeleteAlbumModal';
 import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
 import MenuLibrary from '../MenuLibrary';
@@ -13,7 +13,7 @@ const AlbumDetails = () => {
   console.log("This is albumId:", albumId)
   const sessionUser = useSelector(state => state.session.user);
   const album = useSelector(state => state.albumsReducer[albumId]);
-  console.log("this is album:" ,album)
+  console.log("this is album:", album)
   const [isLoading, setIsLoading] = useState(true);
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
@@ -22,7 +22,7 @@ const AlbumDetails = () => {
     dispatch(getAlbumDetails(albumId)).then(() => setIsLoading(false));
   }, [dispatch, albumId]);
   if (isLoading || !album) return (<>Loading...</>);
-  const {user_id, artist_id, title, lyrics, url, duration, release_date} = album;
+  const { user_id, title, url, release_date, songs, copyright } = album;
 
   const closeMenu = (e) => {
     if (!ulRef.current?.contains(e.target)) {
@@ -30,24 +30,40 @@ const AlbumDetails = () => {
     }
   };
 
+  const canDeleteAlbum = sessionUser && sessionUser.id === user_id;
+
   return (
     <>
-    <div className='albumDetailwrapper'>
-      <div className='albumDetailitem-1'>
+      <div className='albumDetailwrapper'>
+        <div className='albumDetailitem-1'>
           <MenuLibrary />
-      </div>
-      <div className='albumDetailitem-2'>
-        <p className='title'>{title}</p>
-        <p className='lyrics'>{lyrics}</p>
-        <p className='duration'>{duration}</p>
-        <p className='release_date'>{release_date}</p>
-      <button>
-          <OpenModalMenuItem
-            itemText="Delete"
-            onItemClick={closeMenu}
-            modalComponent={<DeleteAlbumModal album = {album}/>}
-          />
-        </button>
+        </div>
+        <div className='albumDetailitem-2'>
+          <p className='title'>{title}</p>
+          <p className='release_date'>{release_date}</p>
+          <p className='url'>{url}</p>
+          {copyright !== null && (
+            <p className='copyright'>{copyright}</p>
+          )}
+          {songs && songs.length > 0 && (
+            <div>
+              <p className='songs'>Songs:</p>
+              <ul>
+                {songs.map((song, index) => (
+                  <li key={index}>{song.title}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {canDeleteAlbum && (
+            <button>
+              <OpenModalMenuItem
+                itemText="Delete"
+                onItemClick={closeMenu}
+                modalComponent={<DeleteAlbumModal album={album} />}
+              />
+            </button>
+          )}
         </div>
       </div>
     </>
--- react-app/src/components/PlaylistDetails/index.js
+++ react-app/src/components/PlaylistDetails/index.js
@@ -6,16 +6,15 @@ import { fetchPlaylistById } from '../../store/playlists';
 import DeletePlaylistModal from '../DeletePlaylistModal';
 import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
 
-
 const PlaylistDetails = () => {
   const dispatch = useDispatch();
   const { playlistId } = useParams();
   const sessionUser = useSelector(state => state.session.user);
   const playlist = useSelector(state => state.playlistsReducer.currentPlaylist);
-  console.log("this is currentplaylist:", playlist)
   const [isLoading, setIsLoading] = useState(true);
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
+
   useEffect(() => {
     dispatch(fetchPlaylistById(playlistId)).then(() => setIsLoading(false));
   }, [dispatch, playlistId]);
@@ -26,25 +25,43 @@ const PlaylistDetails = () => {
     }
   };
 
-  if (isLoading || !playlist) return (<>Loading...</>);
-  const {user_id, artist_id, title, lyrics, url, duration, release_date, songs} = playlist;
+  if (isLoading || !playlist) return <>Loading...</>;
+
+  const { user_id, title, url, description, songs } = playlist;
+
+  const canDeletePlaylist = sessionUser && sessionUser.id === user_id;
+
   return (
     <>
-    <div className='grid-container'>
-      <p className='title'>{title}</p>
-      <p className='lyrics'>{lyrics}</p>
-      <p className='duration'>{duration}</p>
-      <p className='release_date'>{release_date}</p>
-    </div>
-    <button>
-        <OpenModalMenuItem
-          itemText="Delete"
-          onItemClick={closeMenu}
-          modalComponent={<DeletePlaylistModal playlist={playlist}/>}
-        />
-      </button>
+      <div className='grid-container'>
+        <p className='title'>{title}</p>
+        {description !== null && (
+          <p className='description'>{description}</p>
+        )}
+        <p className='url'>{url}</p>
+        {songs && songs.length > 0 && (
+          <div>
+            <p className='songs'>Songs:</p>
+            <ul>
+              {songs.map((song, index) => (
+                <li key={index}>{song.title}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
+      {canDeletePlaylist && (
+        <button>
+          <OpenModalMenuItem
+            itemText="Delete"
+            onItemClick={closeMenu}
+            modalComponent={<DeletePlaylistModal playlist={playlist} />}
+          />
+        </button>
+      )}
     </>
-  )
+  );
 };
 
 export default PlaylistDetails;
