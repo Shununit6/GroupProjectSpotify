08fdff22046ec7195682194e9723182e3561ca08
GaimerGai <devinmrichards95@gmail.com>
2024-01-27 11:35:34 -0700

    Change __init__

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,6 @@ from .artists import Artist
 from .playlists import Playlist
 from .likes import Like
 from .albums import Album
-from .album_songs import album_songs
+from .album_songs import Album_Song
+from .playlist_songs import Playlist_Song
 from .db import environment, SCHEMA
