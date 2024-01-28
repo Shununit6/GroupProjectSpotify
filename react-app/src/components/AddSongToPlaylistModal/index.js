f885a63ff9a99395c4702686247249f4704f9098
GaimerGai <devinmrichards95@gmail.com>
2024-01-28 12:31:41 -0700

    fix add song to playlist remove song from album

--- react-app/src/components/AddSongToPlaylistModal/index.js
+++ react-app/src/components/AddSongToPlaylistModal/index.js
@@ -3,46 +3,63 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { getMyPlaylists, addSongToPlaylistThunk } from '../../store/playlists';
+import { useHistory } from 'react-router-dom';
 
-const AddSongToPlaylistModal = ({song}) => {
+const AddSongToPlaylistModal = ({ song }) => {
     const songId = song.id;
     const dispatch = useDispatch();
-    const playlists = useSelector(state => state.playlistsReducer.playlists);
+    const history = useHistory();
+    const playlists = useSelector(state => Object.values(state.playlistsReducer.playlists.playlists || {}));
     // const playlists = useSelector(state => state.playlistsReducer);
     // console.log('playlists', playlists);
+    const sessionUser = useSelector(state => state.session.user);
     const [isLoading, setIsLoading] = useState(true);
     const [errors, setErrors] = useState({});
-    const {closeModal} = useModal();
+    const { closeModal } = useModal();
 
     useEffect(() => {
         dispatch(getMyPlaylists()).then(() => setIsLoading(false));
-      }, [dispatch]);
+    }, [dispatch]);
 
-    const handleAddSong = async (playlistId) => {
-        dispatch(addSongToPlaylistThunk(playlistId, songId))
-        .then(closeModal)
-        .catch(async (res) => {
+    const isSongInPlaylist = (playlist) => playlist.songs.some((s) => s.id === songId);
+
+    const playlistsByUser = playlists ? playlists.filter(playlist => playlist.user_id === sessionUser?.id) : [];
+
+    const handleAddSong = async (playlistId, playlistTitle) => {
+        try {
+            await dispatch(addSongToPlaylistThunk(playlistId, songId));
+            // Manually update the state after adding the song
+            await dispatch(getMyPlaylists()).then(() => {
+                closeModal();
+                window.confirm(`Song successfully added to "${playlistTitle}"`);
+                history.push(`/playlists/${playlistId}`)
+            });
+        } catch (res) {
             const data = await res.json();
             if (data && data.errors) {
-              setErrors(data.errors);
+                setErrors(data.errors);
             }
-        });
+        }
     };
 
-    if (isLoading ) return (<>Loading...</>);
+        if (isLoading) return (<>Loading...</>);
 
-    return (
-        <div>
-            <p className='heading'>Which playlist would you like to add this song to?</p>
-            <ul>
-                {Object.values(playlists.playlists).map((playlist) => (
-                    <li key={playlist.id}>
-                        <button className='addSongToPlaylist' onClick={() => handleAddSong(playlist.id)}>{playlist.title}</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+        return (
+            <div>
+                <p className='heading'>Which playlist would you like to add this song to?</p>
+                <ul>
+                    {playlistsByUser.map((playlist) => (
+                        !isSongInPlaylist(playlist) && (
+                            <li key={playlist.id}>
+                                <button className='addSongToPlaylist' onClick={() => handleAddSong(playlist.id, playlist.title)}>
+                                    {playlist.title}
+                                </button>
+                            </li>
+                        )
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 
-export default AddSongToPlaylistModal;
+    export default AddSongToPlaylistModal;
--- react-app/src/components/RemoveSongFromPlaylistModal/index.js
+++ react-app/src/components/RemoveSongFromPlaylistModal/index.js
@@ -3,50 +3,57 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { getMyPlaylists, removeSongFromPlaylistThunk } from '../../store/playlists';
+import { useHistory } from 'react-router-dom';
 
-const RemoveSongFromPlaylistModal = ({song}) => {
+const RemoveSongFromPlaylistModal = ({ song }) => {
     const songId = song.id;
     const dispatch = useDispatch();
-    let playlists = useSelector(state => state.playlistsReducer.playlists.playlists);
-    // playlists that have this song:
-    // Object.keys(playlists).forEach(key => {
-    //     if (Array.isArray(playlists[key].songs) && !playlists[key].songs.includes(songId)) {
-    //       return true;
-    //     } else {
-    //       delete playlists[key];
-    //       return false;
-    //     }
-    //   });
-
+    const history = useHistory();
+    const playlists = useSelector(state => Object.values(state.playlistsReducer.playlists.playlists || {}));
+    const sessionUser = useSelector(state => state.session.user);
     const [isLoading, setIsLoading] = useState(true);
     const [errors, setErrors] = useState({});
-    const {closeModal} = useModal();
+    const { closeModal } = useModal();
 
     useEffect(() => {
         dispatch(getMyPlaylists()).then(() => setIsLoading(false));
-      }, [dispatch]);
+    }, [dispatch]);
 
-    const handleDelete = async (playlistId) => {
-        dispatch(removeSongFromPlaylistThunk(playlistId, songId))
-        .then(closeModal)
-        .catch(async (res) => {
+    const isSongInPlaylist = (playlist) => playlist.songs.some((s) => s.id === songId);
+
+    const playlistsByUser = playlists ? playlists.filter(playlist => playlist.user_id === sessionUser?.id) : [];
+
+    const handleRemoveSong = async (playlistId, playlistTitle) => {
+        try {
+            await dispatch(removeSongFromPlaylistThunk(playlistId, songId));
+            // Manually update the state after removing the song
+            await dispatch(getMyPlaylists()).then(() => {
+                closeModal();
+                window.confirm(`Song successfully removed from "${playlistTitle}"`);
+                history.push(`/playlists/${playlistId}`)
+            });
+        } catch (res) {
             const data = await res.json();
             if (data && data.errors) {
-              setErrors(data.errors);
+                setErrors(data.errors);
             }
-        });
+        }
     };
 
-    if (isLoading ) return (<>Loading...</>);
+    if (isLoading) return (<>Loading...</>);
 
     return (
         <div>
             <p className='heading'>Which playlist would you like to delete this song from?</p>
             <ul>
-                {Object.values(playlists).map((playlist) => (
-                    <li key={playlist.id}>
-                        <button className='deleteSongFromPlaylist' onClick={() => handleDelete(playlist.id)}>{playlist.title}</button>
-                    </li>
+                {playlistsByUser.map((playlist) => (
+                    isSongInPlaylist(playlist) && (
+                        <li key={playlist.id}>
+                            <button className='deleteSongFromPlaylist' onClick={() => handleRemoveSong(playlist.id, playlist.title)}>
+                                {playlist.title}
+                            </button>
+                        </li>
+                    )
                 ))}
             </ul>
         </div>
