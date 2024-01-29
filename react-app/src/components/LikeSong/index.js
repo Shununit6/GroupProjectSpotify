4ec0c6c49ea1097ea23c80f1702fa174dad057f1
Shununit6 <unit6xuli@gmail.com>
2024-01-29 10:03:33 -0800

    like/unlike

--- react-app/src/components/LikeSong/index.js
+++ react-app/src/components/LikeSong/index.js
@@ -1,26 +1,27 @@
 import './LikeSong.css';
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSongLikes,createLike, deleteLike } from '../../store/likes';
+import { getSongLikes,getAllLikes,createLike, deleteLike } from '../../store/likes';
 import { getSongDetails } from '../../store/songs';
 
 function LikeSong({songId, userId}) {
     const dispatch = useDispatch();
     const [isLoading, setIsLoading] = useState(true);
-
+    const sessionUser = useSelector(state => state.session.user);
     songId = parseInt(songId);
-    const likes = useSelector(state => state.likesReducer.likes);
+    // const likes = useSelector(state => state.likesReducer.likes);
     const alllikes = useSelector(state => state.likesReducer);
     const numofl=  Object.values(alllikes).filter((curr)=> (curr.song_id == songId)).length;
 
-    const currLike = Object.values(likes).filter((curr)=> (curr.user_id == userId && curr.song_id == songId))
+    let currLike = Object.values(alllikes).filter((curr)=> (curr.song_id == songId && curr.user_id == sessionUser.id));
     // console.log("currLike", currLike);
+    // .filter((curr)=> (curr.song_id == songId));
 
     const[isliked, setIsLiked] = useState(currLike.length==true);
 
     useEffect(() => {
-      dispatch(getSongDetails(songId)).then(()=>dispatch(getSongLikes(songId))).then(() => setIsLoading(false));
-    }, [dispatch, songId, numofl]);
+      dispatch(getSongDetails(songId)).then(()=>dispatch(getAllLikes())).then(()=>dispatch(getSongLikes(songId))).then(() => setIsLoading(false));
+    }, [dispatch, songId]);
 
   if (isLoading) return (<>Loading...</>);
 
@@ -54,13 +55,15 @@ function LikeSong({songId, userId}) {
         <div>{numofl} liked this song</div>
       </>
         )
+  }else{
+    return (
+      <>
+        <button onClick={handleClick}>unlike</button>
+        <div>{numofl} liked this song</div>
+      </>
+    )
   }
-  return (
-    <>
-      <button onClick={handleClick}>unlike</button>
-      <div>{numofl} liked this song</div>
-    </>
-  )
+
 }
 
 export default LikeSong
